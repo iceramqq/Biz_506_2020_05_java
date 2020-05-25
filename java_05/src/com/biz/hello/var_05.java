2f3603c52747f45427a7d97399055ae647ef21c5
iceramqq <iceram22@naver.com>
2020-05-25 13:44:36 +0900

    Raendom

--- java_05/src/com/biz/hello/var_05.java
+++ java_05/src/com/biz/hello/var_05.java
@@ -8,6 +8,7 @@ public class var_05 {
 		int num1=rnd.nextInt(50)+50;
 		int num2=rnd.nextInt(50)+51;
 		
+		//375는 (x+(x*0.1))의 계산에 의해 만들어진 값이다
 		
 	}
 
---
+++ java_06/src/com/biz/hello/var_01.java
@@ -0,0 +1,18 @@
+package com.biz.hello;
+
+import java.util.Random;
+
+public class var_01 {
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		int num = rnd.nextInt(100) + 1;
+		if (num % 3 == 0) {
+			System.out.println(num + "는 3의배수 입니다");
+		}
+		if (num % 5 == 0) {
+			System.out.println(num + "는 5의 배수입니다");
+
+		}
+	}
+
+}
