09b8ba36c33844dd41e43c9b68ec869696dc312b
iceramqq <iceram22@naver.com>
2020-06-06 10:49:58 +0900

    java

--- Java_List/src/com/biz/list/service/ScoreServiceV2.java
+++ Java_List/src/com/biz/list/service/ScoreServiceV2.java
@@ -33,7 +33,7 @@ public class ScoreServiceV2 {
 		System.out.println(line.do_line(listNum));
 		System.out.println("성적리스트");
 		System.out.println(line.do_line(listNum));
-		System.out.println("학번\t국어\t영어\t수학\t음악\t총점\t평균");
+		System.out.println("학번\t국어\t영어\t수학\t총점\t평균");
 		System.out.println(line.single(listNum));
 		int scoreSize = scoreList.size();
 		for (int i = 0; i < scoreSize; i++) {
